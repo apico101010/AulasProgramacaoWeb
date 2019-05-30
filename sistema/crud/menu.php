8695ec515bc909d69e29ba8086f8ce4be80b89bb
apico101010 <42557237+apico101010@users.noreply.github.com>
2019-05-29 21:53:35 -0300

    CRUD - SALAVAR USUARIO - 29/05
    CRUD - SALAVAR USUARIO - 29/05

---
+++ sistema/crud/menu.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+   <?php
+   require_once 'view/head.php';
+   ?>
+</head>
+     <?php
+     require_once "view/head.php";
+     ?>
+
+</head>
+
+
+
+<body>
+<?php
+     require_once "view/nav.php";
+     ?>
+</body>
+
+</html>
